8b896709a93600e24d8268fd755a5e202752e783
tungnv1609 <tungnv@haposoft.com>
2019-07-31 21:39:58 +0700

    CRUD avatar

---
+++ database/migrations/2019_07_24_013628_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->int('nb_phone');
+            $table->string('email');
+            $table->date('b-day');
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
